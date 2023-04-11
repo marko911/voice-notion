c11f1203fb0ce0a29cd828dc76c5f915b36b3caa
Marko Bilal <marko.bilal@hey.com>
2023-04-11 19:03:02 +0200

    bot req log

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -59,6 +59,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log('bot request', req.body)
   await bot.handleUpdate(req.body);
   res.status(200).json({ response: "done" });
 }
