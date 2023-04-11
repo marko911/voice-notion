78bdc6fe6e1b98b847649f1b7e51c3dedacba483
Marko Bilal <marko.bilal@hey.com>
2023-04-11 17:44:11 +0200

    bot log

--- pages/api/server.ts
+++ pages/api/server.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import "@/lib/telegram-bot";
+import bot from "@/lib/telegram-bot";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log("bot boy started", bot);
   res.status(200).json({ response: "server started" });
 }
